55b329305a95234fd5b85d91202e62b5da4567a3
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2023-11-27 17:27:58 +0530

    Update pyDictionary.py

--- PythonDictionary/pyDictionary.py
+++ PythonDictionary/pyDictionary.py
@@ -12,17 +12,17 @@ class PyDictionary:
         
         self.partOfSpeech = partOfSpeech
         
-        meanings = self.getDictionary()[0]["meanings"]
+        meanings = self.getDictionary()
         definitions = []
         synonyms = []
         for x in meanings:
-            if x['partOfSpeech'] == self.partOfSpeech:
-                for y in x['definitions']:
+            if x['meanings'][0]['partOfSpeech'] == self.partOfSpeech:
+                for y in x['meanings'][0]['definitions']:
                     definitions.append(y['definition'])
                     synonyms.append(y['synonyms'])
                     
         return definitions, synonyms
-    
+        
     def getAudio(self):
         
         audios = self.getDictionary()[0]["phonetics"]
