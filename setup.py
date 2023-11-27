51f428c17bb97c634280b875da965cdace02bae6
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2023-11-27 17:31:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setuptools.setup(
     name="PythonDictionary",
-    version="0.0.3",
+    version="0.0.4",
     author="Vishnu Nandakumar",
     author_email="nkumarvishnu25@gmail.com",
     description="Python wrapper for the Free Dictionary API",
